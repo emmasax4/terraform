2e13fbb6b08a1b11fa54f4b7720fd2618032307a
Emma Sax <emma.sax4@gmail.com>
2020-08-22 23:18:44 -0500

    Use a different directory

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  github_token = yamldecode(file("~/.terraform/github_config.yml"))[":github_token"]
+  github_token = yamldecode(file("~/.scripts/github_config.yml"))[":github_token"]
 }
 
 provider "github" {

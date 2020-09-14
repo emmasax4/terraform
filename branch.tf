b638729cf5194a32829d54ba4a97cdb12d7834ff
Emma Sax <emma.sax4+git@gmail.com>
2020-09-14 02:21:49 -0500

    Upgrade terraform version (#6)
    * Add branches to terraform state
    
    * Reformat spacing
    
    * Reformat terraform library too
    
    * Put this back

--- branch.tf
+++ branch.tf
@@ -1,49 +1,49 @@
-# locals {
-#   branches      = distinct(concat(var.additional_branches, [var.default_branch]))
-#   delete_branch = var.default_branch == "master" ? [] : ["master"]
-# }
+locals {
+  branches      = distinct(concat(var.additional_branches, [var.default_branch]))
+  delete_branch = var.default_branch == "master" ? [] : ["master"]
+}
 
-# resource "github_branch" "branch" {
-#   for_each      = toset(local.branches)
-#   repository    = github_repository.repo.name
-#   branch        = each.value
-#   source_branch = var.source_branch
+resource "github_branch" "branch" {
+  for_each      = toset(local.branches)
+  repository    = github_repository.repo.name
+  branch        = each.value
+  source_branch = var.source_branch
 
-#   lifecycle {
-#     ignore_changes        = [branch, source_branch]
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    ignore_changes        = [branch, source_branch]
+    create_before_destroy = true
+  }
+}
 
-# resource "null_resource" "update_default_branch" {
-#   depends_on = [
-#     github_repository.repo,
-#     github_branch.branch
-#   ]
+resource "null_resource" "update_default_branch" {
+  depends_on = [
+    github_repository.repo,
+    github_branch.branch
+  ]
 
-#   triggers = {
-#     default_branch = var.default_branch
-#   }
+  triggers = {
+    default_branch = var.default_branch
+  }
 
-#   provisioner "local-exec" {
-#     command = "curl -H 'Authorization: token ${local.github_token}' --request PATCH --data '{\"default_branch\": \"${var.default_branch}\" }' https://api.github.com/repos/${var.github_owner}/${github_repository.repo.name}"
-#   }
-# }
+  provisioner "local-exec" {
+    command = "curl -H 'Authorization: token ${local.github_token}' --request PATCH --data '{\"default_branch\": \"${var.default_branch}\" }' https://api.github.com/repos/${var.github_owner}/${github_repository.repo.name}"
+  }
+}
 
-# resource "null_resource" "delete_master_branch" {
-#   for_each = setsubtract(toset(local.delete_branch), local.branches)
+resource "null_resource" "delete_master_branch" {
+  for_each = setsubtract(toset(local.delete_branch), local.branches)
 
-#   depends_on = [
-#     github_repository.repo,
-#     github_branch.branch,
-#     null_resource.update_default_branch
-#   ]
+  depends_on = [
+    github_repository.repo,
+    github_branch.branch,
+    null_resource.update_default_branch
+  ]
 
-#   triggers = {
-#     branch_to_delete = "master"
-#   }
+  triggers = {
+    branch_to_delete = "master"
+  }
 
-#   provisioner "local-exec" {
-#     command = "curl -H 'Authorization: token ${local.github_token}' -X DELETE https://api.github.com/repos/${var.github_owner}/${github_repository.repo.name}/git/refs/heads/master"
-#   }
-# }
+  provisioner "local-exec" {
+    command = "curl -H 'Authorization: token ${local.github_token}' -X DELETE https://api.github.com/repos/${var.github_owner}/${github_repository.repo.name}/git/refs/heads/master"
+  }
+}

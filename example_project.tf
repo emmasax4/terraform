711773f6c8392eb0f6b003ffb05d419377efaa3e
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-09-15 23:00:58 -0500

    Adding a bunch of basic terraform

---
+++ example_project.tf
@@ -0,0 +1,29 @@
+module "example_project" {
+  source = "../terraform"
+
+  project_name        = "example_project"
+  description         = "Some random description here"
+  visibility          = "public"
+  additional_branches = ["other-branch"]
+
+  users = {
+    "akw7" = { permission = "developer" }
+  }
+
+  labels = {
+    "these-are-example"   = "0e8a16"
+    "labels-in-different" = "D9534F"
+    "colors"              = "355555"
+  }
+
+  branches_to_protect = {
+    "main" = {
+      push_access  = "developer"
+      merge_access = "maintainer"
+    }
+    "other-branch" = {
+      push_access  = "maintainer"
+      merge_access = "maintainer"
+    }
+  }
+}
